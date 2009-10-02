99dad39d44d6f279798b4e031026647dbe72597e
alexander.drozd <alexander.drozd@648f4d5a-a7be-11de-9ca8-173d21a9ba08>
2009-10-02 09:42:43 +0000

    ms visual studio files

---
+++ trunk/src/main_pocketbook.cpp
@@ -0,0 +1,11 @@
+//description here
+int main (int argc, char * argv[])
+{
+
+	arialb12 = OpenFont("DroidSans", 30, 2);
+//	std::cout<< "Hello, pocketbook" << std::endl;
+	InkViewMain(mainHandler);
+	return 0;
+}
+
+}

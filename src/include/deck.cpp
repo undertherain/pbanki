bcbc27cd35af48344c4df5701e6051eeaebbe1c7
alexander.drozd <alexander.drozd@648f4d5a-a7be-11de-9ca8-173d21a9ba08>
2009-10-15 16:13:50 +0000

    search for deck files in windows

--- src/include/deck.cpp
+++ src/include/deck.cpp
@@ -1,10 +1,11 @@
 #include "deck.hpp"
 #include <string>
 #include <iostream>
+
 #ifdef WIN32
-	
+#include <windows.h>
 #else
-	#include <dirent.h>
+#include <dirent.h>
 #endif
 
 // ----------------- DeckInfo ------------------------
@@ -20,14 +21,43 @@ DeckInfoList Deck::getDeckList()
 {
 	DeckInfoList decks;
 
-#ifdef WIN32
-	
-	decks.push_back(DeckInfo("test1"));
+//	decks.push_back(DeckInfo("test1")); 
+//	decks.push_back(DeckInfo("test2"));
+//	decks.push_back(DeckInfo("test3"));
 
-	decks.push_back(DeckInfo("test2"));
-	decks.push_back(DeckInfo("test3"));
+#ifdef WIN32
+	std::wstring strPattern = L".\\decks\\*.anki"; //будь я проклят! 
+	HANDLE hFile;							 // Handle to file
+	WIN32_FIND_DATA FileInformation;         // File information
+	hFile = ::FindFirstFile(strPattern.c_str(), &FileInformation);
+	if(hFile != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if(FileInformation.cFileName[0] != '.')
+			{
+				if(FileInformation.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+				{
+
+				}
+				else
+				{
+					std::wstring     strFilePathW  = (FileInformation.cFileName); 
+					std::string strFilePath( strFilePathW.begin(), strFilePathW.end() );
+				
+					std::cout<<strFilePath<<std::endl;
+				}
+			}
+		} while(::FindNextFile(hFile, &FileInformation) == TRUE);
+
+	}
+	else 
+	{
+		std::cout<<"coult not finsd decks"<<std::endl;
+	}
+
 #else
-	
+	//linux-specifi
 	struct dirent **filelist;
 	std::string temps;
 	int n;
@@ -43,7 +73,7 @@ DeckInfoList Deck::getDeckList()
 			temps=filelist[ii]->d_name;
 			free (filelist[ii]);
 			if  (temps.find(".anki")!=std::string::npos)
-			decks.push_back(DeckInfo(temps));
+				decks.push_back(DeckInfo(temps));
 			std::cout<<temps<<std::endl;
 			//user *tu= user::load(temps);
 			//pair<userlist::iterator,bool>p=users.insert(make_pair(temps,tu));
@@ -53,9 +83,9 @@ DeckInfoList Deck::getDeckList()
 	}
 #endif
 	std::string sss;
-	
 
-		return decks;
+
+	return decks;
 }
 
 Deck * Deck::loadDeck(DeckId id)

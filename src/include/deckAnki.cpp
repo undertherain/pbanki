70916d02e109520818ccbc622fb75426313c20a2
alexander.drozd <alexander.drozd@648f4d5a-a7be-11de-9ca8-173d21a9ba08>
2009-10-26 16:47:41 +0000

    fetching review cards after no more failed in db

--- src/include/deckAnki.cpp
+++ src/include/deckAnki.cpp
@@ -14,49 +14,66 @@
 
 #define DECK_BUFFER_SIZE 4
 #define MAX_NEW_CARDS 10
-
+#define MAX_FAILED_CARDS 10
+#define FAILED_CARDS_SPACING 20
 
 
 //--------------------------- Fetching Card Queue (buffer) -------------------
 void DeckAnki::Fetch()
 {
-	//start fetching failed cards until we reach buffer limit
-
-
-	std::cout<< "fetching card from  "<< fileName<<std::endl;
-	//std::string query="SELECT * FROM cards where type=0 ORDER BY combinedDue LIMIT " + FormatHelper::ConvertToStr(DECK_BUFFER_SIZE);
-	std::string query="SELECT * FROM cards where combinedDue < " + FormatHelper::GetTimeStr() + " and type=0 and priority in (1,2,3,4) and (not (id in ("+ GetFetchedCardIds() +"))) ORDER BY combinedDue LIMIT 1";
-	//query="select count(id) from cards where  and priority in (1,2,3,4) and type = 0"; 
-
+	//if there are still failed cards to load - load failed card, return
 	try
 	{
-		SQLiteHelper::ExecuteQuery(dbDeck,query);
+
+		if (numCardsFailedInDeck>0)
+		{
+			//start fetching failed cards until we reach buffer limit
+			std::cout<< "fetching failed cards from  "<< fileName<<std::endl;
+			//std::string query="SELECT * FROM cards where type=0 ORDER BY combinedDue LIMIT " + FormatHelper::ConvertToStr(DECK_BUFFER_SIZE);
+			std::string query="SELECT * FROM cards where combinedDue < " + FormatHelper::GetTimeStr() + " and type=0 and priority in (1,2,3,4) and (not (id in ("+ GetFetchedCardIds() +"))) ORDER BY combinedDue LIMIT 1";
+			numCardsFailedInDeck -= FetchCardsByQuery(query);
+			return ;
+		}
+		//if no mo failed cards - start loading review or new cards, until we reach biffer limit
+		std::cout << "fetching due card" << std::endl;
+		std::string query="select * from cards where combinedDue < " + FormatHelper::GetTimeStr() + " and priority in (1,2,3,4) and type = 1 and (not (id in ("+ GetFetchedCardIds() +")))  limit 1";
+		FetchCardsByQuery(query);
+		
+		return ;
+
 	}
 	catch(Exception ex)
 	{
 		std::cout<<"Error in DeckAnki::Fetch: "<<ex.GetMessage()<<std::endl;
 	}
-	std::cout<< "query:  "<< query<<std::endl;
+}
 
-	std::cout<<"Nmum cards fetched = " << SQLiteHelper::values.size() << std::endl;
+int DeckAnki::FetchCardsByQuery(std::string query)
+{
+	SQLiteHelper::ExecuteQuery(dbDeck,query);
+	//std::cout<< "query:  "<< query<<std::endl;
+	//std::cout<<"Num cards fetched = " << SQLiteHelper::values.size() << std::endl;
 	for (unsigned int i=0;i<SQLiteHelper::values.size();i++)
 	{
 		StringMap row = SQLiteHelper::values[i];
 		CardAnki card= CardFromDBRow(row);
-		cardsDueBuffer.push_back(card);
+		cardsDueBuffer.push_front(card);
 		fetchedCardIds.insert(card.id);
 	}
-	//if no mo failed cards - start loading review cards, until we reach biffer limit
-	//if no more review cards - load new cards
+	return SQLiteHelper::values.size();
 }
 
 //--------------------------- Next Card ------------------------------
 ICard DeckAnki::GetNextCard()
 {
 	std::cout<<"getting next card, "<<cardsDueBuffer.size() << " cards in buffer" <<std::endl;
-	if (cardsDueBuffer.size()<DECK_BUFFER_SIZE)
+	if (cardsDueBuffer.size()<MAX_FAILED_CARDS)
 	{
 		Fetch();
+	}
+	else 
+	{
+		std::cout<<"failed cards limit reached, not fetching"<<std::endl;
 	}
 	if (!cardsDueBuffer.empty())
 	{
@@ -84,7 +101,6 @@ void DeckAnki::LoadStats()
 		//new cards total
 		std::string query="select count(id) from cards where type = 2 and priority in (1,2,3,4)";
 		SQLiteHelper::ExecuteQuery(dbDeck,query);
-
 		numCardsNewTotal=FormatHelper::StrToInt(SQLiteHelper::values[0]["count(id)"]);
 		std::cout<<"NEW cards total = "<< numCardsNewTotal <<std::endl;
 
@@ -98,14 +114,14 @@ void DeckAnki::LoadStats()
 		numCardsNewTotal=FormatHelper::StrToInt(SQLiteHelper::values[0]["count(id)"]);
 		std::cout<<"DUE cards today = "<< numCardsNewTotal <<std::endl;
 
-
 		//cards failed today
 		query="select count(id) from cards where combinedDue < " + FormatHelper::GetTimeStr() + " and priority in (1,2,3,4) and type = 0"; 
 		SQLiteHelper::ExecuteQuery(dbDeck,query);
 		numCardsFailedToday=FormatHelper::StrToInt(SQLiteHelper::values[0]["count(id)"]);
 		std::cout<<"FAILED cards today = "<< numCardsFailedToday <<std::endl;
+		numCardsFailedInDeck=numCardsFailedToday;
 
-		//cards revire today
+		//cards review today
 		query="select count(id) from cards where combinedDue < " + FormatHelper::GetTimeStr() + " and priority in (1,2,3,4) and type = 1"; 
 		SQLiteHelper::ExecuteQuery(dbDeck,query);
 		numCardsReviewToday=FormatHelper::StrToInt(SQLiteHelper::values[0]["count(id)"]);
@@ -137,25 +153,7 @@ void DeckAnki::LoadData()
 	//Fetch();
 }
 
-void DeckAnki::AnswerCard(Answer answer)
-{
-	std::cout<<"Registering answer" <<std::endl;
-	//update card stats
-	//calc new interval
 
-	switch (answer)
-	{
-	case 0:
-		cardsDueBuffer.push_back(*lastCard);
-
-		break;
-	default:
-		numCardsFailedToday--;
-		cardsAnsweredBuffer.push_back(*lastCard);
-		break;
-	}
-
-}
 
 DeckAnki::~DeckAnki()
 {
@@ -174,6 +172,9 @@ CardAnki DeckAnki::CardFromDBRow(StringMap row)
 	CardField fldBack(strBack);
 	CardAnki card(fldFront,fldBack);
 	card.type=FormatHelper::StrToInt(row["type"]);
+	card.interval=FormatHelper::StrToFloat(row["interval"]);
+	card.lastInterval=FormatHelper::StrToFloat(row["lastInterval"]);
+	card.factor=FormatHelper::StrToFloat(row["factor"]);
 	card.id=row["id"];
 	//std::cout<<"loaded card id="<<card.id<<std::endl;
 	card.due=FormatHelper::StrToFloat(row["due"]);
@@ -193,9 +194,79 @@ std::string DeckAnki::GetFetchedCardIds()
 	return result;
 }
 
-float DeckAnki::CalcNextInterval(CardAnki card,int ease)
+//-------------------- answering card --------------------
+
+void DeckAnki::AnswerCard(Answer answer)
 {
-	return 0;
+	std::cout<<"Registering answer" <<std::endl;
+	//update card stats
+	//calc new interval
+	float interval=CalcNextInterval(*lastCard,answer);
+	//std::cout<<"\t next interval = " << interval<<std::endl;
+	switch (answer)
+	{
+	case 0:
+		switch (lastCard->type)
+		{
+			case 0: break;
+			case 1: numCardsReviewToday--; numCardsFailedToday++; break;
+			case 2: numCardsNewToday--;  numCardsFailedToday++;  break;
+			default: break;
+		}
+		lastCard->type=0;
+		cardsDueBuffer.push_back(*lastCard);
+		break;
+	default:
+		//decr cards counters
+		switch (lastCard->type)
+		{
+			case 0: numCardsFailedToday--; break;
+			case 1: numCardsReviewToday--; break;
+			case 2: numCardsNewToday--; break;
+			default: break;
+		}
+
+		
+		cardsAnsweredBuffer.push_back(*lastCard);
+		break;
+	}
+}
+
+float DeckAnki::CalcNextInterval(const CardAnki & card,int ease)
+{
+	if (card.type!=1) //failed and new card starts with reseted intervals
+	{
+		switch (ease)
+		{
+		case 0: 
+			return 0;
+			break;
+		case 1:
+			return 1.0;
+			break;
+		case 2:
+			return 4.0;
+			break;
+		case 3:
+			return 6.0;
+			break;
+		default:
+			break;
+		}
+		return 0;	
+	}
+	double factor=card.factor;
+	double newFactor=factor+(0.1-(5-ease)*(0.08+(5-ease)*0.02));
+	newFactor=std::max(1.3,newFactor);
+	float interval;
+	interval = (card.interval==0?1:card.interval);
+	std::cout<<"old interval=" << interval << std::endl;
+	float newInterval;
+	newInterval=interval*newFactor;
+
+
+	std::cout<<"\tfactor="<<factor<<"  new factor="<<newFactor<<"    new interval=" <<newInterval<<std::endl;
+	return newInterval;
 }
 
 
@@ -217,6 +288,56 @@ std::string DeckAnki::GetStatus()
 	strStatus = strStatus + "f:" + FormatHelper::ConvertToStr(GetNumCardsFailedToday())+" r:"+ FormatHelper::ConvertToStr(GetNumCardsReviewToday())+" n:"+ FormatHelper::ConvertToStr(GetNumCardsNewToday());
 	return strStatus;
 }
+
+
+/*
+"Return the next interval for CARD given EASE."
+
+def _nextInterval(self, card, ease):
+delay = self._adjustedDelay(card, ease)
+interval = card.interval
+factor = card.factor
+# if shown early and not failed
+if delay < 0 and card.successive:
+interval = max(card.lastInterval, card.interval + delay)
+if interval < self.midIntervalMin:
+interval = 0
+delay = 0
+# if interval is less than mid interval, use presets
+if ease == 1:
+interval *= self.delay2
+if interval < self.hardIntervalMin:
+interval = 0
+elif interval == 0:
+if ease == 2:
+interval = random.uniform(self.hardIntervalMin,
+self.hardIntervalMax)
+elif ease == 3:
+interval = random.uniform(self.midIntervalMin,
+self.midIntervalMax)
+elif ease == 4:
+interval = random.uniform(self.easyIntervalMin,
+self.easyIntervalMax)
+else:
+# if not cramming, boost initial 2
+if (interval < self.hardIntervalMax and
+interval > 0.166):
+mid = (self.midIntervalMin + self.midIntervalMax) / 2.0
+interval *= (mid / interval / factor)
+# multiply last interval by factor
+if ease == 2:
+interval = (interval + delay/4) * 1.2
+elif ease == 3:
+interval = (interval + delay/2) * factor
+elif ease == 4:
+interval = (interval + delay) * factor * self.factorFour
+fuzz = random.uniform(0.95, 1.05)
+interval *= fuzz
+if self.maxScheduleTime:
+interval = min(interval, self.maxScheduleTime)
+return interval
+
+*/
 //save card
 //remove id from fetchedCardIds
 //

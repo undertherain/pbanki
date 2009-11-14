1991152ede9bfe81af1d5d152f8cc149c785716c
alexander.drozd <alexander.drozd@648f4d5a-a7be-11de-9ca8-173d21a9ba08>
2009-11-14 19:38:31 +0000

    Qt menus\actions

--- src/include/GUI/QT/mainwindow.h
+++ src/include/GUI/QT/mainwindow.h
@@ -3,12 +3,32 @@
 
 #include <QtGui\QMainWindow>
 
+QT_BEGIN_NAMESPACE
+class QAction;
+class QMenu;
+class QPlainTextEdit;
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     MainWindow();
+protected:
+    void closeEvent(QCloseEvent *event);
+private:
+    void createActions();
+    void createMenus();
+	void readSettings();
+
+	QPlainTextEdit *textEdit;
+
+	QMenu *menuFile;
+    QAction *actExit;
+    QAction *actAbout;
+
+
 };
 
 #endif //MAINWINDOW_H
--- src/main_qt.cpp
+++ src/main_qt.cpp
@@ -10,15 +10,10 @@ int main(int argc, char *argv[])
     Q_INIT_RESOURCE(application);
 
     QApplication app(argc, argv);
-//    QLabel *label = new QLabel(QString::fromUtf8("__‘_ - ó_úø!",-1));
-	
-//    QLabel *label = new QLabel(QString("lalala!"));
-//    label->show();
     MainWindow mainWin;
     mainWin.show();
-
     return app.exec();
- 
+
 
 }
   
